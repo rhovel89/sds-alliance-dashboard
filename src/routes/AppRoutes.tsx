2fd9df802ba8685213f86bec40a92c6cf0f634fa
Raymond Hovel <rhovel89@gmail.com>
2026-02-04 21:34:38 -0600

    Fix OAuth flow by adding AuthCallback handler

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/LandingPage";
-import Login from "../pages/Login";
 import AuthCallback from "../pages/AuthCallback";
 import AllianceDashboard from "../pages/AllianceDashboard";
 
@@ -8,10 +7,8 @@ export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<Login />} />
       <Route path="/auth/callback" element={<AuthCallback />} />
       <Route path="/dashboard" element={<AllianceDashboard />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
