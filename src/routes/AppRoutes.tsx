786144f9a7d14e235e142a817036c0b16e284102
Raymond Hovel <rhovel89@gmail.com>
2026-02-05 19:50:02 -0600

    Fix: allow /hq-map inside authenticated routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-
-import LandingPage from '../pages/LandingPage';
-import AuthCallback from '../pages/AuthCallback';
-import Dashboard from '../pages/AllianceDashboard';
-import HQMap from '../pages/HQMap';
-
-import DashboardLayout from '../layouts/DashboardLayout';
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "../pages/LandingPage";
+import AuthCallback from "../pages/AuthCallback";
+import AllianceDashboard from "../pages/AllianceDashboard";
+import HQMap from "../pages/HQMap";
+import AuthGate from "../components/AuthGate";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/auth/callback' element={<AuthCallback />} />
+      {/* Public */}
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/auth/callback" element={<AuthCallback />} />
 
-      <Route element={<DashboardLayout />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-        <Route path='/hq-map' element={<HQMap />} />
+      {/* Authenticated */}
+      <Route element={<AuthGate />}>
+        <Route path="/dashboard" element={<AllianceDashboard />} />
+        <Route path="/hq-map" element={<HQMap />} />
       </Route>
 
-      <Route path='*' element={<div style={{ padding: 40 }}>Fallback Route</div>} />
+      {/* Fallback */}
+      <Route path="*" element={<div style={{ padding: 40 }}>Not Found</div>} />
     </Routes>
   );
 }
