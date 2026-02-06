b79242880f8d25327048eb7be7b4c9590724dd0f
Raymond Hovel <rhovel89@gmail.com>
2026-02-05 20:09:33 -0600

    Fix AuthGate Outlet + routes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -8,17 +8,14 @@ import AuthGate from "../components/AuthGate";
 export default function AppRoutes() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/" element={<LandingPage />} />
       <Route path="/auth/callback" element={<AuthCallback />} />
 
-      {/* Authenticated */}
       <Route element={<AuthGate />}>
         <Route path="/dashboard" element={<AllianceDashboard />} />
         <Route path="/hq-map" element={<HQMap />} />
       </Route>
 
-      {/* Fallback */}
       <Route path="*" element={<div style={{ padding: 40 }}>Not Found</div>} />
     </Routes>
   );
