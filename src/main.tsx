77c5dca343e81734ffd96dfe13f0b16b9912139a
Raymond Hovel <rhovel89@gmail.com>
2026-02-06 21:59:55 -0600

    checkpoint: calendar renders, modal opens, save broken (pre-fix)

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import { AllianceProvider } from './context/AllianceContext';
-
-import "./lib/supabaseClient"; // 🔒 FORCE EARLY INIT
+import AppRoutes from "./routes/AppRoutes";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppRoutes />
     </BrowserRouter>
   </React.StrictMode>
 );
-
