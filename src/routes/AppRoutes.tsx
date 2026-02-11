70e4eb911e9a396a6026ff5b9a9acb15a0401ae5
Raymond Hovel <rhovel89@gmail.com>
2026-02-10 21:24:02 -0600

    fix(owner): restore /owner/select route

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,26 +1,52 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardLayout from "../layouts/DashboardLayout";
 
 import Login from "../pages/Login";
 import AuthCallback from "../pages/AuthCallback";
+
+import DashboardLayout from "../layouts/DashboardLayout";
+import RequireAlliance from "../components/RequireAlliance";
+
 import MyAlliance from "../pages/MyAlliance";
 import EventsPage from "../pages/EventsPage";
+import StateDashboard from "../pages/StateDashboard";
 
-import AllianceHQMap from "../pages/dashboard/AllianceHQMap";
-import PermissionsPage from "../pages/dashboard/Permissions";
+import OwnerDashboard from "../pages/OwnerDashboard";
+import OwnerDashboardSelect from "../pages/OwnerDashboardSelect";
 
 export default function AppRoutes() {
   return (
     <Routes>
+      {/* Public */}
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<AuthCallback />} />
 
+      {/* OWNER */}
+      <Route path="/owner" element={<OwnerDashboard />} />
+      <Route path="/owner/select" element={<OwnerDashboardSelect />} />
+
+      {/* ALLIANCE DASHBOARD */}
       <Route path="/dashboard/:alliance_id" element={<DashboardLayout />}>
-        <Route index element={<MyAlliance />} />
-        <Route path="hq-map" element={<AllianceHQMap />} />
-        <Route path="permissions" element={<PermissionsPage />} />
-        <Route path="events" element={<EventsPage />} />
+        <Route
+          index
+          element={
+            <RequireAlliance>
+              <MyAlliance />
+            </RequireAlliance>
+          }
+        />
+
+        <Route
+          path="events"
+          element={
+            <RequireAlliance>
+              <EventsPage />
+            </RequireAlliance>
+          }
+        />
       </Route>
+
+      {/* STATE */}
+      <Route path="/state/1" element={<StateDashboard />} />
     </Routes>
   );
 }
