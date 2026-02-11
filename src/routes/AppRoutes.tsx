8669c9676f2384f5cc5300a606a70b20e462686c
Raymond Hovel <rhovel89@gmail.com>
2026-02-10 22:23:55 -0600

    fix(routes): restore /owner/select without touching HQ map

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,26 +1,38 @@
 import { Routes, Route } from "react-router-dom";
-import DashboardLayout from "../layouts/DashboardLayout";
 
 import Login from "../pages/Login";
 import AuthCallback from "../pages/AuthCallback";
+
+import DashboardLayout from "../layouts/DashboardLayout";
 import MyAlliance from "../pages/MyAlliance";
 import EventsPage from "../pages/EventsPage";
 
+import OwnerDashboardSelect from "../pages/OwnerDashboardSelect";
+import OwnerDashboard from "../pages/OwnerDashboard";
+
 import AllianceHQMap from "../pages/dashboard/AllianceHQMap";
-import PermissionsPage from "../pages/dashboard/Permissions";
+import Permissions from "../pages/dashboard/Permissions";
 
 export default function AppRoutes() {
   return (
     <Routes>
+
+      {/* PUBLIC */}
       <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<AuthCallback />} />
 
+      {/* OWNER */}
+      <Route path="/owner" element={<OwnerDashboard />} />
+      <Route path="/owner/select" element={<OwnerDashboardSelect />} />
+
+      {/* ALLIANCE DASHBOARD */}
       <Route path="/dashboard/:alliance_id" element={<DashboardLayout />}>
         <Route index element={<MyAlliance />} />
         <Route path="hq-map" element={<AllianceHQMap />} />
-        <Route path="permissions" element={<PermissionsPage />} />
+        <Route path="permissions" element={<Permissions />} />
         <Route path="events" element={<EventsPage />} />
       </Route>
+
     </Routes>
   );
 }
