6d75dd0b750c6d73ac0c7e5f28458a0278f7e269
Raymond Hovel <rhovel89@gmail.com>
2026-02-15 10:31:54 -0600

    Fix App layout: render AuthRedirector + TopNav + routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import AppRoutes from './routes/AppRoutes';
+import AppRoutes from "./routes/AppRoutes";
 import TopNav from "./components/nav/TopNav";
 import AuthRedirector from "./components/auth/AuthRedirector";
 
 export default function App() {
-  return <AuthRedirector />
+  return (
+    <>
+      <AuthRedirector />
       <TopNav />
-      <AppRoutes />;
+      <AppRoutes />
+    </>
+  );
 }
-
