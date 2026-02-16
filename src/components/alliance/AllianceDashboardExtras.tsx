69c7a4679f671e03ee1fe6aab0c74bedb28cc102
Raymond Hovel <rhovel89@gmail.com>
2026-02-16 00:10:58 -0600

    Features: alliance announcements + guides (fixed migration + pages)

---
+++ src/components/alliance/AllianceDashboardExtras.tsx
@@ -0,0 +1,11 @@
+import AllianceAnnouncementsPanel from "../announcements/AllianceAnnouncementsPanel";
+import AllianceGuidesPanel from "../guides/AllianceGuidesPanel";
+
+export default function AllianceDashboardExtras() {
+  return (
+    <>
+      <AllianceAnnouncementsPanel />
+      <AllianceGuidesPanel />
+    </>
+  );
+}
