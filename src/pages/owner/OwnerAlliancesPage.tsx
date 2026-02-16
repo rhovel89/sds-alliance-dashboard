35138f6bf87d4dff5e33dcace60de3fd6c9a2f21
Raymond Hovel <rhovel89@gmail.com>
2026-02-16 14:13:11 -0600

    Fix: dedupe OwnerAlliances helpers (UUID_RE)

--- src/pages/owner/OwnerAlliancesPage.tsx
+++ src/pages/owner/OwnerAlliancesPage.tsx
@@ -93,8 +93,6 @@ async function resolveStateIdForAlliances(
 }
 
 
-const UUID_RE =
-  /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 let _cachedStateUuid: string | null = null;
 
