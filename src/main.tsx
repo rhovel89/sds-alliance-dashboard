ed74ae043d3dc9305373e77707233488247297c1
Raymond Hovel <rhovel89@gmail.com>
2026-02-19 23:54:01 -0600

    Add global crash guard (error boundary overlay + crash bundle)

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 import './index.css';
+import AppErrorBoundary from "./components/system/AppErrorBoundary";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <AppErrorBoundary>
+    <React.StrictMode>
     <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
-  </React.StrictMode>
+      </React.StrictMode>
+  </AppErrorBoundary>
 );
 import './styles/zombie-theme.css';
 import './styles/zombie-base.css';
