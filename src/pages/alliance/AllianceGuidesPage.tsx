999d4a747c1931da543cd78a5add7a847c1879af
Raymond Hovel <rhovel89@gmail.com>
2026-02-23 23:36:09 -0600

    feat: guides media uploader (file picker + drag/drop + paste)

--- src/pages/alliance/AllianceGuidesPage.tsx
+++ src/pages/alliance/AllianceGuidesPage.tsx
@@ -1,5 +1,12 @@
 import { AllianceGuidesCommandCenter } from "./AllianceGuidesCommandCenter";
+import GuideMediaUploader from "../../components/guides/GuideMediaUploader";
+import { useParams } from "react-router-dom";
 
 export default function AllianceGuidesPage() {
+  // alliance_id param is used as alliance code in your current URLs (example: /dashboard/WOC/guides)
+  const { alliance_id } = useParams();
+  const allianceCode = String(alliance_id ?? "");
+
   return <AllianceGuidesCommandCenter />;
 }
+
