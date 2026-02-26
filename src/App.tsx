74ed2e6db84d9afdb24a2f8cd01855d30345d96a
Raymond Hovel <rhovel89@gmail.com>
2026-02-26 00:45:02 -0600

    feat: show player names in uuid dropdowns (permissions/linking/onboarding)

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
 import MobileSidebarToggle from "./components/nav/MobileSidebarToggle";
 import MobileTopbarToggle from "./components/nav/MobileTopbarToggle";
+import UuidOptionLabelEnhancer from "./components/players/UuidOptionLabelEnhancer";
 
 export default function App() {
   return (
@@ -24,10 +25,12 @@ export default function App() {
       <FloatingLanguageSwitcher />
       <MobileSidebarToggle />
       <MobileTopbarToggle />
+      <UuidOptionLabelEnhancer />
     </>
     </I18nextProvider>
   );
 }
+
 
 
 
