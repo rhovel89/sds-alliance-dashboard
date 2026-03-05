e3354f127ae42b4a91dcb4d96fc2402e7d1be5e1
Raymond Hovel <rhovel89@gmail.com>
2026-03-05 09:29:35 -0600

    fix(ui): owner command center wrapper imports correct legacy owner page

--- src/pages/owner/OwnerCommandCenterHomePage.tsx
+++ src/pages/owner/OwnerCommandCenterHomePage.tsx
@@ -1,14 +1,20 @@
 import React, { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
+
 import CommandCenterShell from "../../components/commandcenter/CommandCenterShell";
 import { getCommandCenterModules } from "../../components/commandcenter/ccModules";
-import LegacyOwnerHome from "../pages/owner/OwnerHomePage";
+
+import LegacyOwnerHome from "";
 
 export default function OwnerCommandCenterHomePage() {
   const nav = useNavigate();
   const cc = useMemo(() => getCommandCenterModules(), []);
   const modules = useMemo(() => cc.map(({ key, label, hint }) => ({ key, label, hint })), [cc]);
-  function onSelectModule(k: string) { const to = cc.find(m => m.key === k)?.to; if (to) nav(to); }
+
+  function onSelectModule(k: string) {
+    const to = cc.find((m) => m.key === k)?.to;
+    if (to) nav(to);
+  }
 
   return (
     <CommandCenterShell
